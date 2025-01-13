8450afc4459d53fb1d2e2a438a7a9441bc6c947e
Anna Nikiforovskaya <nikiannanik@gmail.com>
2025-01-13 16:14:40 +0100

    Change facts

--- class01-git/facts.py
+++ class01-git/facts.py
@@ -9,9 +9,12 @@ people = [
            facts=['Fact-right.', 'Fact-write.', 'Fact-right.']),
     Person(github_login='javanochka', first_name='Anna', last_name='Nikiforovskaja',
            facts=['I can meow like a cat', 'I am a PhD student', 'I am not Estonian']),
+    Person(github_login="bla", first_name='Bla', last_name="Bla", 
+           facts=['Meow', 'Woof', 'Quack']),
 ]
 # facts are numerated from 0
 answers = {'example': 1,
+           'bla': 0,
            'javanochka': 2,}
 
 
